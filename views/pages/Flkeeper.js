377f313ddf914a07097c035dac2702872afd5f15
4567q <44399930+4567q@users.noreply.github.com>
2021-03-05 19:24:25 +0400

    fix 2

--- views/pages/Flkeeper.js
+++ views/pages/Flkeeper.js
@@ -2,112 +2,149 @@ let Flkeeper = {
     render : async () => {
         let view =  /*html*/`
                        
+        <div class="row">    
         <div class="col-sm">
         <div class="card mb-3">
-        <div class="card-header"><center><span style="color: #fff;">file keeper</span><br><a href="https://4567q.github.io/starter.page/">
-         back</a></center></div>
+        <div class="card-header"><center><span style="color: #fff;">software</span><br><a href="https://4567q.github.io/starter.page/">back</a></center></div>
         
         <div class="card-body">
         <!------------------------->
-         
-         <div class="container">
+        
+        <div class="container">
          <div class="row row-cols-1 row-cols-sm-2 row-cols-md-6">
-          <div class="col">
-            
-          <span style="color: #fff;">
-            <center>
-            <b>app:</b><br>
-            <b>autostat.exe</b><br>
-            <b><a href="files/autostat.exe">download</a></b>
-          </center>
-          </span>
+            <div class="col">
+             
+                   <a class="button" href="https://vk.com/" target="_blank" style='text-decoration: none;'>
+                    <div class="alert alert-light" role="alert">
+                    <img src="src/icon/vk_32x32.png" class="rounded-0">
+                    <span>autostatus</span> 
+                    </div>
+                    </a>
 
-          </div>
-          <div class="col">
-            
-          <span style="color: #fff;">
-            <center>
-            <b>app:</b><br>
-            <b>boostM.exe</b><br>
-            <b><a href="https://cloud.mail.ru/public/2Ju2/4vSpfp99C" target="_blank">download</a></b>
-          </center>
-          </span>
+            </div>
+            <div class="col">
 
-          </div>
-          <div class="col">
-            
-          <span style="color: #fff;">
-            <center>
-            <b>app:</b><br>
-            <b>FSbot.exe</b><br>
-            <b><a title="license for Premori Ove" href="https://cloud.mail.ru/public/3EDj/4sFPcty1G" target="_blank">download</a></b>
-          </center>
-          </span>
+             <a class="button" href="https://www.instagram.com/?hl=ru" target="_blank" style='text-decoration: none;'>
+        <div class="alert alert-light" role="alert">
+        <img src="src/icon/ins_32x32.png" class="rounded-0">
+        <span>boostM</span>
+        </div>
+        </a>
 
-          </div>
-          <div class="col">
-            
-           <span style="color: #fff;">
-            <center>
-            <b>app:</b><br>
-            <b>TGpars.exe</b><br>
-            <b><a href="https://github.com/elizhabs/TGPars" target="_blank" >download</a></b>
-          </center>
-          </span>
+            </div>
+            <div class="col">
 
-          </div>
+            <a class="button" href="https://web.telegram.org/#/login" target="_blank" style='text-decoration: none;'>
+        <div class="alert alert-light" role="alert">
+        <img src="src/icon/tg_32x32.png" class="rounded-0">
+        <span>FSbot</span>
+        </div>
+        </a>
 
-         <div class="col">
-            
-           <span style="color: #fff;">
-            <center>
-            <b>app:</b><br>
-            <b>TGpars.exe</b><br>
-            <b><a href="https://github.com/elizhabs/TGPars" target="_blank" >download</a></b>
-          </center>
-          </span>
+            </div>
+            <div class="col">
 
-          </div>
+           <a class="button" href="https://2ch.hk/" target="_blank" style='text-decoration: none;'>
+        <div class="alert alert-light" role="alert">
+        <img src="src/icon/dv_32x32.png" class="rounded-0">
+        <span>TGpars</span>
+        </div>
+        </a>
 
-          <div class="col">
-            
-           <span style="color: #fff;">
-            <center>
-            <b>app:</b><br>
-            <b>TGpars.exe</b><br>
-            <b><a href="https://github.com/elizhabs/TGPars" target="_blank" >download</a></b>
-          </center>
-          </span>
+            </div>
+            <div class="col">
 
-          </div>
+             <a class="button" href="https://soundcloud.com/premori" target="_blank" style='text-decoration: none;'>
+        <div class="alert alert-light" role="alert">
+        <img src="src/icon/sc_32x32.png" class="rounded-0">
+        <span>Mclips</span>
+        </div>
+        </a>
 
-          <div class="col">
-            
-           <span style="color: #fff;">
-            <center>
-            <b>app:</b><br>
-            <b>TGpars.exe</b><br>
-            <b><a href="https://github.com/elizhabs/TGPars" target="_blank" >download</a></b>
-          </center>
-          </span>
+            </div>
+            <div class="col">
 
-          </div>
+             <a class="button" href="https://www.looperman.com/loops" target="_blank" style='text-decoration: none;'>
+        <div class="alert alert-light" role="alert">
+        <img src="src/icon/lp_32x32.png" class="rounded-0">
+        <span>Fbot</span>
+        </div>
+        </a> 
 
-          <div class="col">
-            
-           <span style="color: #fff;">
-            <center>
-            <b>app:</b><br>
-            <b>TGpars.exe</b><br>
-            <b><a href="https://github.com/elizhabs/TGPars" target="_blank" >download</a></b>
-          </center>
-          </span>
-
-          </div>
-
+            </div>
         </div>
         </div>
 
+        <div class="container">
+         <div class="row row-cols-1 row-cols-sm-2 row-cols-md-6">
+            <div class="col">
+             
+            <a class="button" href="https://open.spotify.com/" target="_blank" style='text-decoration: none;'>
+        <div class="alert alert-light" role="alert">
+        <img src="src/icon/sp_32x32.png" class="rounded-0">
+        <span>Vkviews</span>
+        </div>
+        </a>
+
+            </div>
+            <div class="col">
+
+           <a class="button" href="https://cloud.mail.ru/public/1qv5/y9zcNyNhR" target="_blank" style='text-decoration: none;'>
+        <div class="alert alert-light" role="alert">
+        <img src="src/icon/fc_32x32.png" class="rounded-0">
+        <span>ATRep</span>   
+        </div>
+        </a>
+
+            </div>
+            <div class="col">
+
+            <a class="button" href="https://github.com/Code-Building/ImGuiBuilder" target="_blank" style='text-decoration: none;'>
+        <div class="alert alert-light" role="alert">
+        <img src="src/icon/gh_32x32.png" class="rounded-0">
+        <span>Bchets</span>
+        </div>
+        </a>
+
+            </div>
+            <div class="col">
+        <a class="button" href="" target="_blank" style='text-decoration: none;'>
+        <div class="alert alert-light" role="alert">    
+        <img src="src/icon/qw_32x32.png" class="rounded-0">
+        <span>BMbot</span>
+        </div>
+        </a>
+
+            </div>
+            <div class="col">
+
+        <a class="button" href="https://online-audio-converter.com/ru/" target="_blank" style='text-decoration: none;'>
+        <div class="alert alert-light" role="alert">
+        <img src="src/icon/acn_32x32.png" class="rounded-0">
+        <span>AudioC</span>
+        </div>
+        </a>
+
+            </div>
+            <div class="col">
+
+        <a class="button" href="https://yandex.ru/maps/225/russia/?l=sat%2Cskl&ll=99.505405%2C61.698653&z=3" target="_blank" style='text-decoration: none;'>
+        <div class="alert alert-light" role="alert">
+        <img src="src/icon/ym_32x32.png" class="rounded-0">
+        <span>YMaps</span>
+        </div>
+        </a>
+        </div>
+        </div>
+        </div>
+        
+        <!------------------------->
+        
+       
+        <!------------------------->
+        </div>
+        </div>
+        </div>
 
 
            
